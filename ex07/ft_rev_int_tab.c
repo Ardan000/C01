556163a282c06aea41393f4eb77c9a1666989299
sfaugere <satnislasf@orange.fr>
2022-02-12 17:44:23 +0100

    re

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -7,7 +7,7 @@ void ft_rev_int_tab(int *tab, int size)
     int a = 0;    //a = 0
     int b = size - 1;   // b = 6
     int swap;    
-    while(b > (size/2))      // 6 < 7/2
+    while(b > ((size-1)/2))      // 6 < 7/2
     {   
         swap = tab[a];      // c = 1
         tab[a] = tab[b];  // tab[a] = 96876546
@@ -17,7 +17,7 @@ void ft_rev_int_tab(int *tab, int size)
     }
 }
 
-/*void print_my_tab(int* tab, int size)
+void print_my_tab(int* tab, int size)
 {
     int i = 0;
     while(i < size)
@@ -30,9 +30,8 @@ printf("\n");
 
 int main()
 {
-    int my_tab[] = {1, 5, -8, 654, 5464635, -46548647, 96876546};
-    print_my_tab(my_tab, 7);
-    ft_rev_int_tab(my_tab, 7);
-    print_my_tab(my_tab, 7);
+    int my_tab[] = {-69114, 692777, 54};
+    print_my_tab(my_tab, 3);
+    ft_rev_int_tab(my_tab, 3);
+    print_my_tab(my_tab, 3);
 }
-*/
